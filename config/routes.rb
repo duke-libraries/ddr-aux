23ecc889aa584f25e01a1803db22174c9e971824
dchandekstark <dchandekstark@gmail.com>
2015-04-29 21:35:18 -0400

    AdminEntitiesController: - Added index action - Changed show action to slug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/manage', as: 'rails_admin'
-  get '/admin_entities/:slug(.:format)', to: 'admin_entities#show'
+
+  get '/admin_entities/:slug(.:format)', to: 'admin_entities#slug', defaults: { format: "json" }
+  resources :admin_entities, only: :index, defaults: { format: "json" }
+
 end
--- spec/routing/admin_entities_routing_spec.rb
+++ spec/routing/admin_entities_routing_spec.rb
@@ -2,6 +2,13 @@ require "rails_helper"
 
 RSpec.describe "AdminEntity routing", type: :routing do
   it "should have a route that finds the record by slug and returns JSON" do
-    expect(get: '/admin_entities/foobar.json').to route_to(controller: "admin_entities", action: "show", slug: "foobar", format: "json")
+    @route = {controller: "admin_entities", action: "slug", slug: "foobar", format: "json"}
+    expect(get: '/admin_entities/foobar.json').to route_to(@route)
+    expect(get: '/admin_entities/foobar').to route_to(@route)
+  end
+  it "should have an index router that returns JSON" do
+    @route = {controller: "admin_entities", action: "index", format: "json"}
+    expect(get: '/admin_entities.json').to route_to(@route)
+    expect(get: '/admin_entities').to route_to(@route)
   end
 end
