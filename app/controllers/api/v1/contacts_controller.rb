9980b0073bcec0d89bc26cfec2ab5cb48238d2c4
David Chandek-Stark <dchandekstark@gmail.com>
2015-11-23 11:22:44 -0500

    Refactored for better compatibility with ActiveResource.
    ModelController#index processes query params so ActiveResource models
    can use .where.
    Individual model controllers and routes removed.
    ModelController#find deprecated in favor of #index with params.

--- app/controllers/api/v1/contacts_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api::V1
-  class ContactsController < ModelController
-
-    def find_params
-      params.permit(:slug, :name)
-    end
-
-  end
-end
---
+++ spec/controllers/api/v1/model_controller_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+module Api::V1
+  RSpec.describe ModelController do
+
+    models = %w( admin_sets contacts licenses )
+
+    before(:all) { AdminSet.create(code: "foo", title: "Foo") }
+
+    after(:all) { AdminSet.destroy_all }
+
+    models.each do |model|
+
+      describe "#index" do
+        describe model do
+          it "succeeds" do
+            get :index, model: model
+            expect(response.response_code).to eq(200)
+            expect(response.content_type).to eq("application/json")
+          end
+        end
+      end
+
+      describe "#show" do
+        describe model do
+          it "succeeds when found" do
+            get :show, model: model, id: 1
+            expect(response.response_code).to eq(200)
+            expect(response.content_type).to eq("application/json")
+            result = JSON.parse(response.body)
+            expect(result).to be_a(Hash)
+            expect(result["id"]).to eq(1)
+          end
+          it "returns an error response when not found" do
+            get :show, model: model, id: "foo"
+            expect(response.response_code).to eq(404)
+            expect(response.content_type).to eq("application/json")
+          end
+        end
+      end
+    end
+
+  end
+end
