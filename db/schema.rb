886a386c8ce7c8b74ea683899cf9212790abaf62
David Chandek-Stark <dchandekstark@gmail.com>
2016-02-26 18:18:30 -0500

    Version 2.0.0.rc1

--- db/schema.rb
+++ db/schema.rb
@@ -13,43 +13,43 @@
 
 ActiveRecord::Schema.define(version: 20160223014043) do
 
-  create_table "admin_sets", force: true do |t|
-    t.string   "code"
-    t.string   "title"
+  create_table "admin_sets", force: :cascade do |t|
+    t.string   "code",       limit: 255
+    t.string   "title",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "contacts", force: true do |t|
-    t.string "slug"
-    t.string "name"
-    t.string "short_name"
-    t.string "url"
-    t.string "phone"
-    t.string "email"
-    t.string "ask"
+  create_table "contacts", force: :cascade do |t|
+    t.string "slug",       limit: 255
+    t.string "name",       limit: 255
+    t.string "short_name", limit: 255
+    t.string "url",        limit: 255
+    t.string "phone",      limit: 255
+    t.string "email",      limit: 255
+    t.string "ask",        limit: 255
   end
 
-  create_table "licenses", force: true do |t|
-    t.string   "title"
-    t.string   "url"
+  create_table "licenses", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "url",        limit: 255
     t.text     "terms",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "email",              default: "",    null: false
-    t.string   "encrypted_password", default: "",    null: false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "display_name"
-    t.string   "nickname"
+  create_table "users", force: :cascade do |t|
+    t.string   "username",           limit: 255
+    t.string   "email",              limit: 255, default: "",    null: false
+    t.string   "encrypted_password", limit: 255, default: "",    null: false
+    t.string   "first_name",         limit: 255
+    t.string   "last_name",          limit: 255
+    t.string   "display_name",       limit: 255
+    t.string   "nickname",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",              default: false
-    t.string   "api_key"
+    t.boolean  "admin",                          default: false
+    t.string   "api_key",            limit: 255
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
