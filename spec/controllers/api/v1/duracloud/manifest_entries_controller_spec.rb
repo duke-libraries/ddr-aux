05b34d7491b09e09e6fb5b13e51fa64f23c3a4e0
David Chandek-Stark <dchandekstark@gmail.com>
2016-08-23 17:14:50 -0400

    Version 2.1.0.rc6

--- spec/controllers/api/v1/duracloud/manifest_entries_controller_spec.rb
+++ spec/controllers/api/v1/duracloud/manifest_entries_controller_spec.rb
@@ -5,14 +5,14 @@ module Api::V1
     RSpec.describe ManifestEntriesController do
 
       describe "required params" do
-        it "requires 'content_id'" do
-          expect { get :index, space_id: 'foo' }.to raise_error(ActionController::ParameterMissing)
+        it "requires 'object_uri'" do
+          expect { get :fcrepo3, space_id: 'foo' }.to raise_error(ActionController::ParameterMissing)
         end
         it "requires 'space_id'" do
-          expect { get :index, content_id: 'foo' }.to raise_error(ActionController::ParameterMissing)
+          expect { get :fcrepo3, object_uri: 'info:fedora/duke:1' }.to raise_error(ActionController::ParameterMissing)
         end
         specify {
-          expect { get :index, space_id: 'foo', content_id: 'bar' }.not_to raise_error
+          expect { get :fcrepo3, space_id: 'foo', object_uri: 'info:fedora/duke:1' }.not_to raise_error
         }
       end
 
