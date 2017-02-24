b2746bbf8ecc3f554dbbbb0125b65425d4ea44c8
David Chandek-Stark <dchandekstark@gmail.com>
2017-02-24 15:37:43 -0500

    Rolls back breaking changes in 613607dd8db5f763139c5d24c0209597ed557a83.
    VERSION reset to 2.3.0.pre.
    Bug fixed in IdentifiersController related to https://github.com/duke-libraries/ezid-client/issues/71.

--- spec/routing/api/v1/duracloud_routing_spec.rb
+++ spec/routing/api/v1/duracloud_routing_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe "API V1 duracloud routing" do
 
-  it "routes POST /api/duracloud/manifest_entries/select" do
-    expect(post: "/api/duracloud/manifest_entries/select")
-      .to route_to(controller: "api/v1/duracloud/manifest_entries", action: "select", format: "json")
+  it "routes GET /api/duracloud/manifest_entries" do
+    expect(get: "/api/duracloud/manifest_entries/fcrepo3.json?space_id=foo&object_uri=info%3Afedora%2Fduke%3A1")
+      .to route_to(controller: "api/v1/duracloud/manifest_entries", action: "fcrepo3", space_id: "foo", object_uri: "info:fedora/duke:1", format: "json")
   end
 
 end
