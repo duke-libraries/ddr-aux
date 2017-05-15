d8b729a4892046d5a09cf08d73e9e6e54d984fa3
Cory Lown <cory.lown@duke.edu>
2017-05-15 13:45:32 -0400

    Add ISO 639-2 language codes and labels

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -59,6 +59,21 @@ RailsAdmin.config do |config|
     end
   end
 
+  config.model 'Language' do
+    list do
+      sort_by :code
+      field :code do
+        sort_reverse false
+      end
+      field :label
+    end
+    update do
+      configure :code do
+        read_only true
+      end
+    end
+  end
+
   config.model 'License' do
     list do
       field :title
--- spec/routing/api/v1/model_routing_spec.rb
+++ spec/routing/api/v1/model_routing_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe "API v1 model routing", type: :routing do
       .to route_to(controller: "api/v1/model", action: "index", model: "admin_sets", format: "json")
     expect(get: "/api/contacts")
       .to route_to(controller: "api/v1/model", action: "index", model: "contacts", format: "json")
+    expect(get: "/api/languages")
+      .to route_to(controller: "api/v1/model", action: "index", model: "languages", format: "json")
     expect(get: "/api/licenses")
       .to route_to(controller: "api/v1/model", action: "index", model: "licenses", format: "json")
     expect(get: "/api/foobar").not_to be_routable
@@ -17,6 +19,8 @@ RSpec.describe "API v1 model routing", type: :routing do
       .to route_to(controller: "api/v1/model", action: "show", model: "admin_sets", id: "1", format: "json")
     expect(get: "/api/contacts/1")
       .to route_to(controller: "api/v1/model", action: "show", model: "contacts", id: "1", format: "json")
+    expect(get: "/api/languages/1")
+      .to route_to(controller: "api/v1/model", action: "show", model: "languages", id: "1", format: "json")
     expect(get: "/api/licenses/1")
       .to route_to(controller: "api/v1/model", action: "show", model: "licenses", id: "1", format: "json")
   end
@@ -26,6 +30,8 @@ RSpec.describe "API v1 model routing", type: :routing do
       .to route_to(controller: "api/v1/model", action: "find", model: "admin_sets", code: "dc", format: "json")
     expect(get: "/api/contacts/find?code=dc")
       .to route_to(controller: "api/v1/model", action: "find", model: "contacts", code: "dc", format: "json")
+    expect(get: "/api/languages/find?code=cym")
+      .to route_to(controller: "api/v1/model", action: "find", model: "languages", code: "cym", format: "json")
     expect(get: "/api/licenses/find?url=https%3A%2F%2Fcreativecommons.org%2Fpublicdomain%2Fzero%2F1.0%2F")
       .to route_to(controller: "api/v1/model", action: "find", model: "licenses", url: "https://creativecommons.org/publicdomain/zero/1.0/", format: "json")
   end
